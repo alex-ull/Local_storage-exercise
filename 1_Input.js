b7580ec7fd7cb70bca1a4653ac4202e8aa9f9bc6
Alexander Ulland <alex.ulland02@gmail.com>
2024-11-26 14:17:26 +0100

    1 26.11

---
+++ 1_Input.js
@@ -0,0 +1,39 @@
+let out = document.getElementById("content");
+let hey = document.getElementById("greeting");
+
+window.onload = wakeup() 
+    
+function wakeup() {
+    let fname = localStorage.getItem("firstname");
+    let lname = localStorage.getItem("lastname");
+    if (fname === null && lname === null) {
+        hey.innerHTML = `Would you like to register?`
+        out.innerHTML = `<button onclick="change()">Add name</button>`;
+    }
+    else {   
+        hey.innerHTML = `Welcome back ${fname} ${lname}`;
+        out.innerHTML = `<button onclick="change()">Change name</button>`;
+    }
+        
+        
+}
+
+function change() {
+    hey.innerHTML = `Register name`;
+    out.innerHTML = `
+        <input type="text" id="firstname" placeholder="Firstname..." required>
+        <input type="text" id="lastname" placeholder="Lastname..." required>
+        <button onclick="conf()">Confirm</button>
+        <button onclick="wakeup()">Cancel</button>
+        `;
+}
+
+function conf() {
+    let fname = document.getElementById("firstname").value;
+    let lname = document.getElementById("lastname").value;
+
+    localStorage.setItem("firstname", fname);
+    localStorage.setItem("lastname", lname);
+
+
+}
